7d836b9608352e94d8de5362598561808c2ad96d
diddntjd99 <diddntjd99@gmail.com>
2021-05-12 22:30:07 +0900

    안드로이드 <-> node.js 테스트

--- app/src/main/java/com/example/HSB/MainActivity.java
+++ app/src/main/java/com/example/HSB/MainActivity.java
@@ -3,16 +3,61 @@ package com.example.HSB;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
 
 public class MainActivity extends AppCompatActivity {
+    public Button button;
+    public TextView textView;
+    private Socket socket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void test() {
-        //hihihi
+        try {
+            socket = IO.socket("http://119.192.49.237:32/");
+            socket.connect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        textView = (TextView) findViewById(R.id.textView);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                socket.emit("book_name", "야수가 간다1");
+
+                socket.on("return", new Emitter.Listener() {
+                    @Override
+                    public void call(final Object... args) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    JSONObject data = (JSONObject) args[0];
+                                    textView.setText(data.getString("title"));
+
+                                    //Toast.makeText(MainActivity.this, "학번을 다시 입력해주세요.", Toast.LENGTH_SHORT).show();
+                                } catch(Exception e) {
+                                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                    }
+                });
+            }
+        });
     }
 }
